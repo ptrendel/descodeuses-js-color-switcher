5e9d97aab091e091d2f77a84a8af2d75b7eaa874
Pierre Trendel <pierre.trendel@gmail.com>
2023-10-04 11:41:55 +0200

    Add an random number generator function

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const color = ["red", "blue", "green"];
 function setBgColor() {
   document.getElementById("bg").style.backgroundColor = color[0];
   document.getElementById("bgcolor").innerHTML = color[0];
+  console.log(color.length);
 }
+
+function getRandomNumber() {
+  console.log(Math.floor(Math.random() * color.length));
+}
+
+getRandomNumber();
 
 document.getElementById("bgbutton").addEventListener("click", setBgColor);
